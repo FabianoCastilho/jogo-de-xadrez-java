cc1f41cbd42d2ea7f3d8a6603ede3e7cc14537bc
Fabiano Castilho <fabiano_castilho@yahoo.com.br>
2022-07-05 22:47:23 -0300

    Criado classes Posição, Tabuleiro, Peça, Partida e Peça de Xadrez e exibição do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
+import xadrez.PartidaDeXadrez;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		PartidaDeXadrez partida = new PartidaDeXadrez();
+		UI.printTabuleiro(partida.getPecas());
 
 	}
 
