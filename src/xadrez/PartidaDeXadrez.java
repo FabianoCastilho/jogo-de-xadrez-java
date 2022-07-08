17bc1916b236f0ae95eb3cb0da3d22a2d579c015
Fabiano Castilho <fabiano_castilho@yahoo.com.br>
2022-07-08 14:35:34 -0300

    Melhoria na exibição do tabuleiro e Peças

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -1,36 +1,47 @@
 package xadrez;
 
-import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
 
 public class PartidaDeXadrez {
-	
+
 	private Tabuleiro tabuleiro;
-	
+
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
 		configuracaoInicial();
 	}
-	
+
 	public PecaDeXadrez[][] getPecas() {
-		PecaDeXadrez[][] matriz = new PecaDeXadrez[tabuleiro.getLinhas()][ tabuleiro.getColunas()];
+		PecaDeXadrez[][] matriz = new PecaDeXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
 		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
-			for(int j = 0; j < tabuleiro.getColunas(); j++) {
-				matriz[i][j] = (PecaDeXadrez) tabuleiro.peca(i, j);		
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				matriz[i][j] = (PecaDeXadrez) tabuleiro.peca(i, j);
 			}
 		}
 		return matriz;
 	}
+
 	private void colocarNovaPeca(char coluna, int linha, PecaDeXadrez peca) {
 		tabuleiro.colocarPeca(peca, new PosicaoDoXadrez(coluna, linha).converteParaPosicao());
-		
 	}
+
 	private void configuracaoInicial() {
-		colocarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.BRANCO));
-		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.PRETO));
+
+		colocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+		colocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 
 }
